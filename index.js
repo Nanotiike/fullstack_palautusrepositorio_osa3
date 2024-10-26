d2c8a199514290ca63fa81b3a4b29174761f781f
Harri <harri.gavert@helsinki.fi>
2024-10-26 17:59:11 +0300

    3.22

--- index.js
+++ index.js
@@ -1,112 +1,124 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
 const app = express()
-var morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json())
-
-const cors = require('cors')
 
 app.use(cors())
 
 app.use(express.static('dist'))
 
 let persons = [
-      { 
-        name: "Arto Hellas", 
-        number: "040-123456",
-        id: "1"
-      },
-      { 
-        name: "Ada Lovelace", 
-        number: "39-44-5323523",
-        id: "2"
-      },
-      { 
-        name: "Dan Abramov", 
-        number: "12-43-234345",
-        id: "3"
-      },
-      { 
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122",
-        id: "4"
-      }
-    ]
-
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
-
-  app.get('/api/info', (request, response) => {
-    const date = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>`)
-  })
-
-  app.get('/api/persons/:id', (request, response) => {
-    const id = (request.params.id)
-    const person = persons.find(person => person.id === id)
-  
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-
-  const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: '1'
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: '2'
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: '3'
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: '4'
   }
-  
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
+]
 
-    if (!body.number) {
-        return response.status(400).json({ 
-            error: 'number missing' 
-        })
-    }
-    
-    if (persons.find(person => person.name === body.name)) {
-      return response.status(400).json({ 
-        error: 'name must be unique' 
-      })
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId(),
-    }
-  
-    persons = persons.concat(person)
-  
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = (request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
     response.json(person)
-  })
+  } else {
+    response.status(404).end()
+  }
+})
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (body.name.length <= 3) {
+    return response.status(400).json({
+      error: 'name must be at least 3 characters long'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  if (body.number.length <= 8 || !body.number.includes('-')) {
+    return response.status(400).json({
+      error: 'number must be at least 8 characters long and must be in two parts seperated by a dash'
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
